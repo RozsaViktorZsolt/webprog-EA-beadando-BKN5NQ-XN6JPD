3051641a60ef4c68e5b3d6fda764cafe01de543a
RozsaViktorZsolt <viktorrozsa05@gmail.com>
2026-04-18 14:54:27 +0200

    Update get_adatok.php

--- get_adatok.php
+++ get_adatok.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 // A tárhelyed adataival töltsd ki! [cite: 59, 60]
-$dbh = new PDO('mysql:host=localhost;dbname=nbibeadando', 'nbibeadando', 'HasználjonErősJelszót',
+$dbh = new PDO('mysql:host=localhost;dbname=nbibeadando', 'nbibeadando', 'XN6JPD',
 array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
 $stmt = $dbh->query("SELECT * FROM klub"); // Kezdésnek a klubokat kérjük le
